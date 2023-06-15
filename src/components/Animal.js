246801e79afa95c408baf4bb0fa6f866f9df09a3
Edith <mxedithbaltazar@gmail.com>
2023-06-14 18:33:08 -0700

    updated components

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -1,12 +1,14 @@
 //The responsibilty of this component is to be a resuable UI element that displays an Animals name, species, photo
+// add age, photo(optional?) -- or default?
 
 import './Animal.css';
 
-const Animal = () => {
-    return (<section className="Animal">
-            <h3>Animal name: Willow</h3>
-            <p>Species: Cat</p>
-            <img src='https://placekitten.com/g/200/300' ></img>
+const Animal = (props) => {
+    return (
+    <section className="Animal">
+            <h3>Animal name: { props.name } (cute!)</h3>
+            <p>Species: { props.species }</p>
+            <img src='https://placekitten.com/g/200/300' alt='Photo of cat' ></img>
         </section>);
 };
 
--- src/components/AnimalList.js
+++ src/components/AnimalList.js
@@ -3,17 +3,82 @@ import './AnimalList.css'
 import Animal from './Animal';
 
 
-const AnimalList = () => {
+// const AnimalList = () => {
+
+//     const firstAnimal = {
+//         name: "violet",
+//         species: "pitbull mix"
+
+//     };
+//     const secondAnimal = {
+//         name: "norman",
+//         species: "puppy"
+
+//     };
+//     const thirdAnimal = {
+//         name: "juni",
+//         species: "poodle"
+// 
+    // };
+// wont see for loop method
+    // const renderedAnimals = [];
+    // for (const creture of listOfAnimals) {
+    //     renderedAnimals.push (
+    //         <li> <Animal 
+    //         name={creature.name}
+    //         species={creature.species}
+    //     />
+    //      </li>
+    //     )
+    // };
+
+   // const renderedAnimals = listOfAnimals.map(creature => (
+
+            //{/*  <li> <Animal 
+               // name={creature.name}
+                // species={creature.species}
+            // />
+        // </li> */}
+
+        // console.log({ renderedAnimals }); ));
+
+// The responsibility of this component is to be a reusable UI element that displays an Animal's
+    const Animal = (props) => {
+
+
     return (<section className="AnimalList">
-        <h2>Animal List</h2>
-        <ul className="AnimalsList__list">
-        <li><Animal></Animal></li>
-        <li><Animal></Animal></li>
-        <li><Animal></Animal></li>
-        <li><Animal></Animal></li>
-        </ul>
-    </section>)
+        {/* <h2>Animal List</h2> */}
+        <h3>Name: { props.name } (cute!)</h3>
+        <p>Species: { props.species }</p>
+        { props.photo ? <img src={ props.photo } alt="Photo of Willow"></img> : "[No Photo]" }
+    </section>);
 };
+        {/* <li><Animal name="Violet" species="pit mix"></Animal></li> */}
+        {/* <li> */}
+            {/* <Animal 
+            // name={firstAnimal.name}
+            // species={firstAnimal.species} 
+        // /> */}
+         {/* {
+            listOfAnimals.map(creature => (
+                <Animal 
+                    name={firstAnimal.name}
+                    species={firstAnimal.species} 
+                 /> 
+                
+        <li><Animal></Animal></li>
+        <li><Animal></Animal></li>
+
+            )) */}
+            
+    Animal.propTypes = {
+        name: PropTypes.string,
+        species: PropTypes.string.isRequired,
+        adopted: PropTypes.bool,
+        age: PropTypes.number,
+        photo: PropTypes.string
+    }
+            
 
 export default AnimalList;
 
